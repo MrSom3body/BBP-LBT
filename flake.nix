f6fce0014a35798bc120ba2197947c1c39d6ef59
Karun Sandhu <karun@sndh.dev>
2025-12-14 19:34:00 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,115 @@
+{
+  description = "Flake using uv2nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs = {
+        pyproject-nix.follows = "pyproject-nix";
+        uv2nix.follows = "uv2nix";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+
+      forEachSystem = f: lib.genAttrs (import inputs.systems) (system: f pkgsFor.${system});
+      pkgsFor = lib.genAttrs (import inputs.systems) (
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+
+      workspace = inputs.uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+
+      # Extend generated overlay with build fixups
+      #
+      # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
+      # This is an additional overlay implementing build fixups.
+      # See:
+      # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+      pyprojectOverrides = _final: _prev: {
+        # Implement build fixups here.
+        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+      };
+
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      python = pkgs.python313;
+
+      pythonSet =
+        (pkgs.callPackage inputs.pyproject-nix.build.packages {
+          inherit python;
+        }).overrideScope
+          (
+            lib.composeManyExtensions [
+              inputs.pyproject-build-systems.overlays.default
+              overlay
+              pyprojectOverrides
+            ]
+          );
+    in
+    {
+      formatter = forEachSystem (pkgs: pkgs.nixfmt-tree);
+
+      packages = forEachSystem (_pkgs: {
+        default = pythonSet.mkVirtualEnv "python-env" workspace.deps.default;
+      });
+
+      devShells = forEachSystem (
+        pkgs:
+        import ./nix/shell.nix {
+          inherit
+            self
+            lib
+            pkgs
+            python
+            ;
+        }
+      );
+
+      checks = forEachSystem (
+        pkgs:
+        import ./nix/checks.nix {
+          inherit inputs;
+          inherit pkgs;
+        }
+      );
+    };
+}
---
+++ nix/checks.nix
@@ -0,0 +1,26 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+  pre-commit-check = inputs.git-hooks-nix.lib.${pkgs.stdenv.hostPlatform.system}.run {
+    src = ../.;
+    hooks = {
+      # nix
+      deadnix.enable = true;
+      nil.enable = true;
+      nixfmt-rfc-style.enable = true;
+      statix.enable = true;
+
+      # markdown
+      markdownlint = {
+        enable = true;
+        settings.configuration = {
+          line-length.tables = false;
+          no-inline-html = false;
+        };
+      };
+    };
+  };
+}
